cd6ced7c93191cfacc6f1f8ce3b09872bdf75ee7
averiedayimshufflin <aahn.official@gmail.com>
2026-06-25 17:07:50 -0400

    Made wireframe pc landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,17 @@
-import Background from "@/img/image.png";
-import Monitor from "@/img/give.png";
-import Pcbox from "@/img/pc_box.png";
+"use client"
+import Desktop from "@/img/desktop.png";
 import Image from "next/image";
 
+import { useRouter } from "next/navigation";
+import {Button} from "@/components/ui/button";
 export default function Page() {
+
+    const router = useRouter();
+  
   return (
-    <div
-      className="fixed inset-0 bg-no-repeat flex items-center justify-center"
-      style={{
-        backgroundImage: `url(${Background.src})`,
-        backgroundSize: "2500px 1250px",
-      }}
-    >
-      {/* GAME WORLD */}
-      <div className="relative w-[1920px] h-[1080px]">
-
-        <Image
-          src={Monitor}
-          alt="monitor"
-          style={{
-            position: "absolute",
-            width: 1500,
-            height: 1150,
-            left: 0,
-            top: 20,
-          }}
-        />
-
-        <Image
-          src={Pcbox}
-          alt="pcbox"
-          style={{
-            position: "absolute",
-            width: 1000,
-            height: 1300,
-            left: 1100,
-            top: -100,
-          }}
-        />
-
-      </div>
-    </div>
+   <div className="flex flex-col items-center justify-center h-screen gap-4">
+    <Image src = {Desktop} alt = "desktop" width={500} height={500}/>
+    <Button onClick={() => router.push("/login")}>Want to turn on the pc?</Button>
+   </div>
   );
 }
